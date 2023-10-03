19ed9d234dffb42b09cb5485fac1a8fcec1d3d6b
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-02 20:27:23 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -16,5 +16,11 @@ app.use((req, res, next) => {
     })
 })
 
+// Configurar CORS para permitir todas las solicitudes (en un entorno de desarrollo local)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Esto permite todas las solicitudes
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 export default app;
