29b0bce3685f31ca555c270561ef9d65e337cdc0
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-02 20:33:16 -0600

    cors4

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import negociosRoutes from './routes/negocios.routes.js';
 import indexRoutes from './routes/index.routes.js';
 import adminRoutes from './routes/admin.routes.js';
 
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
@@ -16,11 +18,5 @@ app.use((req, res, next) => {
     })
 })
 
-// Configurar CORS para permitir todas las solicitudes (en un entorno de desarrollo local)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Esto permite todas las solicitudes
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 export default app;
