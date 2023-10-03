2d3a8407b7c925db8e6e2ea32471d6af5165d31b
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-02 21:26:11 -0600

    cor11

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -29,3 +29,14 @@ export const login = async (req, res) => {
         });
     }
 };
+
+export const getAdmin = async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM admin')
+        res.json(rows)
+    } catch (error) {        
+        res.status(500).json({
+            message: 'Error al obtener el admin'
+        })
+    }
+}
