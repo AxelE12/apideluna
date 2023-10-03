308e5d55c8fe664d1f91ded17ed169003698cab9
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-02 20:52:27 -0600

    cors9

--- src/app.js
+++ src/app.js
@@ -2,11 +2,9 @@ import express from 'express';
 import negociosRoutes from './routes/negocios.routes.js';
 import indexRoutes from './routes/index.routes.js';
 import adminRoutes from './routes/admin.routes.js';
-import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
@@ -17,6 +15,14 @@ app.use((req, res, next) => {
         message: 'Not found'
     })
 })
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method)')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+}) 
 
 
 export default app;
