9f3711aef65afd901c60c659005a765229dfafea
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-02 19:38:39 -0600

    cors2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import app from './app.js';
 import {PORT} from './config.js';
 
-const express = require('express');
-const app = express();
-
-// Configurar CORS para permitir todas las solicitudes (en un entorno de desarrollo local)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Esto permite todas las solicitudes
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
 app.listen(PORT);
 console.log('Server on port', PORT);
