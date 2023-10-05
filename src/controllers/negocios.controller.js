01fb688b678db462f586585691d31ed3400c6c7e
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-05 12:06:00 -0600

    imagen

--- src/controllers/negocios.controller.js
+++ src/controllers/negocios.controller.js
@@ -1,92 +1,5 @@
 import {pool} from '../db.js';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
-});
-
-const upload = multer({ storage: storage });
-
-// Controlador para crear un nuevo negocio con carga de imagen
-export const crearNegocio = async (req, res) => {
-    try {
-        // Primero, maneja la carga de imagen
-        upload.single('imagenNegocio')(req, res, async function (err) {
-            if (err instanceof multer.MulterError) {
-                return res.status(400).json({ message: 'Error al cargar la imagen' });
-            } else if (err) {
-                return res.status(500).json({ message: 'Error interno del servidor' });
-            }
-
-            // Después de cargar la imagen, puedes continuar con la inserción en la base de datos
-            const {
-                tituloNegocio,
-                disponible,
-                distancia,
-                imagenCategoria,
-                descripcion,
-                insignia,
-                tipoNegocio,
-                direccion,
-                imagenRealNegocio,
-                nombreCategoria,
-                horario,
-                latitud,
-                longitud,
-            } = req.body;
-
-            const rutaImagenNegocio = 'uploads/' + req.file.filename;
-
-            const [rows] = await pool.query(
-                'INSERT INTO negocios (imagenNegocio, tituloNegocio, disponible, distancia, imagenCategoria, descripcion, insignia, tipoNegocio, direccion, imagenRealNegocio, nombreCategoria, horario, latitud, longitud) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                [
-                    rutaImagenNegocio,
-                    tituloNegocio,
-                    disponible,
-                    distancia,
-                    imagenCategoria,
-                    descripcion,
-                    insignia,
-                    tipoNegocio,
-                    direccion,
-                    imagenRealNegocio,
-                    nombreCategoria,
-                    horario,
-                    latitud,
-                    longitud,
-                ]
-            );
-
-            res.send({
-                id: rows.insertId,
-                imagenNegocio: rutaImagenNegocio,
-                tituloNegocio,
-                disponible,
-                distancia,
-                imagenCategoria,
-                descripcion,
-                insignia,
-                tipoNegocio,
-                direccion,
-                imagenRealNegocio,
-                nombreCategoria,
-                horario,
-                latitud,
-                longitud,
-            });
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error al crear el negocio',
-        });
-    }
-};
-
-
 
 export const getNegocios = async (req, res) => {
     try {
@@ -115,7 +28,7 @@ export const getNegocio = async (req, res) => {
     }
 }
 
-/*
+
 export const crearNegocio = async (req, res) => {
     try {
         const {imagenNegocio, tituloNegocio, disponible, distancia, imagenCategoria, descripcion, insignia, tipoNegocio, direccion, imagenRealNegocio, nombreCategoria, horario, latitud, longitud} = req.body;
@@ -132,7 +45,7 @@ export const crearNegocio = async (req, res) => {
     }
 }
 
-*/
+
 
 export const eliminarNegocio = async (req, res) =>{
     try {
