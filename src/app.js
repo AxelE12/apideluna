e77d7aea350d65406c3234888ba759c0b66ce255
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-08 20:29:39 -0600

    negimg

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(fileUpload());
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PATCH, DELETE');
     next();
