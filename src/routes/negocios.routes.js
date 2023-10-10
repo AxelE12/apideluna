5520b1bba80be521bc00d0f8f8143aada5fb6eba
AxelE12 <104877127+AxelE12@users.noreply.github.com>
2023-10-10 14:04:22 -0600

    firebase

--- src/routes/negocios.routes.js
+++ src/routes/negocios.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {getNegocios, getNegocio, crearNegocio, actualizarNegocio, eliminarNegocio, imagenNegocio, imagenCategoria, imagenRealNegocio, getImagenN, getImagenC, getImagenRN} from '../controllers/negocios.controller.js';
+import {getNegocios, getNegocio, crearNegocio, actualizarNegocio, eliminarNegocio} from '../controllers/negocios.controller.js';
 
 const router = Router();
 
@@ -8,18 +8,6 @@ router.get('/negocios', getNegocios);
 router.get('/negocios/:id', getNegocio);
 
 router.post('/negocios', crearNegocio);
-
-router.post('/imagenNegocio', imagenNegocio);
-
-router.get('/getImagenNegocio', getImagenN);
-
-router.post('/imagenCategoria', imagenCategoria);
-
-router.get('/getImagenCategoria', getImagenC);
-
-router.post('/imagenRealNegocio', imagenRealNegocio);
-
-router.get('/getImagenRealNegocio', getImagenRN);
 
 router.delete('/negocios/:id', eliminarNegocio);
 
